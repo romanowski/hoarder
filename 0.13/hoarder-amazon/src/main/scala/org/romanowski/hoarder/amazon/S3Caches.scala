98950737a5fae318661b04c1648b3fc5d486f897
Krzysztof Romanowski <romanowski.kr@gmail.com>
2017-06-17 23:30:37 +0200

    various s3 related fixes

--- 0.13/hoarder-amazon/src/main/scala/org/romanowski/hoarder/amazon/S3Caches.scala
+++ 0.13/hoarder-amazon/src/main/scala/org/romanowski/hoarder/amazon/S3Caches.scala
@@ -10,18 +10,36 @@ trait S3Caches extends DownloadableCacheSetup {
 
   def bucketName: String
 
+  def prefix: String
+
+  def dirLikePrefix = if(prefix.endsWith("/")) prefix else s"$prefix/"
+
   protected def s3ClientBuilder = AmazonS3ClientBuilder.standard()
 
   protected def transferManagerBuilder = TransferManagerBuilder.standard().withS3Client(s3ClientBuilder.build())
 
   /** Loads cache to temporary directory */
-  override def downloadCache(to: Path): Unit = transferManagerBuilder.build()
-    .downloadDirectory(bucketName, "", to.toFile)
-    .waitForCompletion()
+  override def downloadCache(to: Path): Path = {
+    transferManagerBuilder.build()
+      .downloadDirectory(bucketName, dirLikePrefix, to.toFile)
+      .waitForCompletion()
+    to.resolve(dirLikePrefix)
+  }
 
 
   /** Stores cache from temporary directory */
   override def uploadCache(from: Path): Unit = transferManagerBuilder.build()
-    .uploadDirectory(bucketName, "", from.toFile, /*includeSubdirectories =*/ true)
+    .uploadDirectory(bucketName, dirLikePrefix, from.toFile, /*includeSubdirectories =*/ true)
     .waitForCompletion()
+
+  /** Remove current cache entry for this job. Called before new cache is exported */
+  override def invalidateCache(): Unit = {
+    import collection.JavaConverters._
+    val s3 = s3ClientBuilder.build()
+    s3.listObjects(bucketName, prefix).getObjectSummaries.asScala.foreach {
+      summary =>
+        s3.deleteObject(bucketName, summary.getKey)
+    }
+
+  } // We don't need to invalidate since upload will override old entries
 }
