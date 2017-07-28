0f3d47586db04380173164d97149f9449c4b24da
Krzysztof Romanowski <romanowski.kr@gmail.com>
2017-07-28 20:07:12 +0200

    Cross build to 0.13.16 and fix s3 integration test.

--- ci-scripts/install-and-clone.sh
+++ ci-scripts/install-and-clone.sh
@@ -1,6 +1,4 @@
-cd 0.13 && \
-  sbt "set version.in(Global) :=\"1.0-for-test\"" publishLocal && \
-  cd .. && \
+sbt "set version.in(Global) :=\"1.0-for-test\"" publishLocal && \
   echo simulate stash workflow, first clone && \
   git clone -b hoarder-ci https://github.com/romanowski/ensime-server.git test-ws && \
   echo 'addSbtPlugin("com.github.romanowski" % "hoarder" % "1.0-for-test")' > test-ws/project/hoarder.sbt && \
