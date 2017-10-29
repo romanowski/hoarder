338a8da287a027d6b374e0afeaf73ff875aa0532
Krzysztof Romanowski <romanowski.kr@gmail.com>
2017-10-29 09:21:48 +0100

    Fix hoarder integration tests.
    There is no easy way to test travis workflow - so tests was changed.

--- ci-scripts/integrationTest.sh
+++ ci-scripts/integrationTest.sh
@@ -6,7 +6,6 @@ export HOARDER_CI_VERSION="1.0.1-a-test-version"
 sbt '^publishLocal' && \
     ls -alR test-ws
     ci-scripts/install-and-clone.sh ensime-server && \
-    cp ci-scripts/HoarderS3Test.scala test-ws\project
     cd test-ws  && \
     echo object IntegrationTest extends org.romanowski.hoarder.tests.IntegrationTestFlowBase > project/IntegrationTest.scala
     sbt postBuild && \
--- ci-scripts/s3-integration.sh
+++ ci-scripts/s3-integration.sh
@@ -4,6 +4,7 @@ export HOARDER_CI_VERSION="1.0.1-a-test-version"
 
 sbt ^publishLocal && \
     ci-scripts/install-and-clone.sh ensime-server && \
+    cp ci-scripts/HoarderS3Test.scala test-ws/project && \
     cd test-ws  && \
     echo 'addSbtPlugin("com.github.romanowski" %% "hoarder-amazon" % "'$HOARDER_CI_VERSION'")' > project/hoarder-amazon.sbt && \
     sbt preventCompilationStatus && \
