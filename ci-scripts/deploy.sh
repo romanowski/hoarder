fca9e148bcccc3e02cb10ea4036d1a29659a9fe2
Krzysztof Romanowski <romanowski.kr@gmail.com>
2018-02-18 19:58:07 +0100

    Fix deploy script

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$scriptName" = 'sbtTests-1.0' ]; then
      openssl aes-256-cbc -K $encrypted_6c92e0c7ed61_key -iv $encrypted_6c92e0c7ed61_iv -in private-files.zip.enc -out private-files.zip -d
      unzip private-files.zip
      sbt ^publishSigned finalizeRelease
