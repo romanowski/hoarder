4843e2a21106a2492b98fe51d13409018ae7e0e9
Krzysztof Romanowski <romanowski.kr@gmail.com>
2019-03-20 00:30:22 +0100

    Add logging to deployment

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$scriptName" = 'sbtTests-1.0' ]; then
-     openssl aes-256-cbc -K $encrypted_6c92e0c7ed61_key -iv $encrypted_6c92e0c7ed61_iv -in private-files.zip.enc -out private-files.zip -d
+
+if ([ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_TAG" != "" ]) && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$scriptName" = 'sbtTests-1.0' ]; then
+     echo Deploying...
+     openssl aes-256-cbc -K $encrypted_6c92e0c7ed61_key -iv $encrypted_6c92e0c7ed61_iv -in cred.zip.enc -out private-files.zip -d
      unzip private-files.zip
      sbt ^publishSigned finalizeRelease
+else
+     echo Deployment skipped: TB="$TRAVIS_BRANCH" TT="$TRAVIS_TAG" PR="$TRAVIS_PULL_REQUEST" SN="$scriptName"
 fi
