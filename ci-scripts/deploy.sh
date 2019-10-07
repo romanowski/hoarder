b1722f34a3a8e214492f98ab660a0ae7655da186
Krzysztof Romanowski <romanowski.kr@gmail.com>
2019-10-07 14:54:55 +0200

    Update deploy.sh

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -3,7 +3,7 @@
 if ([ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_TAG" != "" ]) && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$scriptName" = 'sbtTests-1.0' ]; then
      echo Deploying...
      openssl aes-256-cbc -K $encrypted_6c92e0c7ed61_key -iv $encrypted_6c92e0c7ed61_iv -in private-files.zip.enc -out private-files.zip -d
-     unzip private-files.zip
+     unzip -o private-files.zip
      sbt ^publishSigned finalizeRelease
 else
      echo Deployment skipped: TB="$TRAVIS_BRANCH" TT="$TRAVIS_TAG" PR="$TRAVIS_PULL_REQUEST" SN="$scriptName"
